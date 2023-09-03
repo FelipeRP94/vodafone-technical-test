68c42aa346c4eb4d0865327ad730b521dee58d1f
Felipe Ruiz Pinto <felipe.r.p.1994@gmail.com>
2023-09-03 22:29:39 +0200

    Configure toast

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,14 @@
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import { ApolloProvider } from "@apollo/client";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { client } from "./lib/apollo-client/apollo.config";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "@mui/material";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import { theme } from "./lib/material-ui/theme";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -18,6 +20,7 @@ root.render(
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <App />
+          <ToastContainer />
         </ThemeProvider>
       </ApolloProvider>
     </BrowserRouter>
