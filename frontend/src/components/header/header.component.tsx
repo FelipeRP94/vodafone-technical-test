58c5d83b1f9dc405888bccbd63146124735d736b
Felipe Ruiz Pinto <felipe.r.p.1994@gmail.com>
2023-09-03 13:21:23 +0200

    Add navigation in header

--- frontend/src/components/header/header.component.tsx
+++ frontend/src/components/header/header.component.tsx
@@ -3,15 +3,19 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import logo from "../../assets/img/vodafone-logo.png";
 import { HeaderContainer, Logo } from "./header.styles";
+import { useNavigate } from "react-router-dom";
+import { routes } from "../../lib/react-router/constants";
 
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <HeaderContainer>
       <Toolbar>
         <Logo src={logo} alt="Vodafone logo" />
         <Box>
-          <Button>Home</Button>
-          <Button>Devices</Button>
+          <Button onClick={() => navigate(routes.HOME)}>Home</Button>
+          <Button onClick={() => navigate(routes.DEVICES)}>Devices</Button>
         </Box>
       </Toolbar>
     </HeaderContainer>
